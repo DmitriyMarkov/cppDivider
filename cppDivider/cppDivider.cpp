439ed1222ec2cc3fb442383786cceda67bea2f3d
Дмитрий <@DIMA-PC>
2020-05-19 19:28:56 +0500

    Учтены отрицательные значения чисел и деление на ноль

--- cppDivider/cppDivider.cpp
+++ cppDivider/cppDivider.cpp
@@ -9,12 +9,39 @@ int main()
 	std::cin >> a;
 	std::cout << "Введите делитель: ";
 	std::cin >> b;
-	int t = 0;
-	while (a > b)
+	if (b == 0)
 	{
-		a = a - b;
-		t++;
+		std::cout << "Нельзя делить на ноль!" << std::endl;
+		system("pause"); //Чтобы программа не закрылась сразу после вывода
+		return -1;
 	}
+	int t = 0;
+	if (a > 0)
+		if (b > 0)
+			while (a >= b)
+			{
+				a -= b;
+				t++;
+			}
+		else
+			while (a >= -b)
+			{
+				a += b;
+				t--;
+			}
+	else
+		if (b > 0)
+			while (-a >= b)
+			{
+				a += b;
+				t--;
+			}
+		else
+			while (a <= b)
+			{
+				a -= b;
+				t++;
+			}
 	std::cout << "Целая часть: " << t << std::endl;
 	std::cout << "Остаток: " << a << std::endl;
 	system("pause"); //Чтобы программа не закрылась сразу после вывода
